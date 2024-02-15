41ac9df2bcab68c08e8e01ca8924155977737224
Benjamin Ng <60500272+benymng@users.noreply.github.com>
2024-02-15 14:09:47 +0000

    Now dynamically displays readme on the '/' endpoint

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 var cors = require("cors");
 const bodyParser = require("body-parser");
+const fs = require('fs');
+const path = require('path');
+const { marked } = require('marked');
 
 app.use(cors());
 
@@ -23,7 +26,14 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  fs.readFile(path.join(__dirname, 'README.md'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.sendStatus(500);
+    }
+    const html = marked(data);
+    res.send(html);
+  });
 });
 
 app.use("/users", require("./routes/users"));
