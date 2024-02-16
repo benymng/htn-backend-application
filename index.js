8643e61ed8887a311ed902f984a75669630af725
Benjamin Ng <60500272+benymng@users.noreply.github.com>
2024-02-16 15:29:15 +0000

    Added more documentation for index and groups

--- index.js
+++ index.js
@@ -7,41 +7,50 @@ const fs = require('fs');
 const path = require('path');
 const { marked } = require('marked');
 
+// Enable CORS for all requests
 app.use(cors());
 
+// Set common headers for all responses
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
   next();
 });
 
+// Parse JSON bodies and URL encoded bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// dynamically set port for server
 let port = process.env.PORT || 3001;
+// MongoDB connection URI
 const db = require("./config/key").mongoURI;
 
+// Connect to MongoDB
 mongoose
   .connect(db)
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
+// Serve README.md as HTML at the root route
 app.get("/", (req, res) => {
   fs.readFile(path.join(__dirname, 'README.md'), 'utf8', (err, data) => {
     if (err) {
-      console.error(err);
-      return res.sendStatus(500);
+      console.error(err); // Log file read error
+      return res.sendStatus(500); // Internal Server Error
     }
-    const html = marked(data);
-    res.send(html);
+    const html = marked(data); // Convert Markdown to HTML
+    res.send(html); // Send the HTML as the response
   });
 });
 
+// Route grouping
 app.use("/users", require("./routes/users"));
 app.use("/skills", require("./routes/skills"));
 app.use("/groups", require("./routes/groups"));
 
+// Start the server
 app.listen(port, () => {
   console.log(`Listening on port http://localhost:${port}`);
 });
 
-module.exports = app;
+module.exports = app; // for testing
--- routes/groups.js
+++ routes/groups.js
@@ -2,10 +2,19 @@ const express = require("express");
 const router = express.Router();
 const Group = require("../models/group");
 const { User } = require("../models/user");
-const axios = require("axios");
 const { generateUsername } = require("unique-username-generator");
 const { Pinecone } = require("@pinecone-database/pinecone");
 
+/**
+ * Configures and returns a Pinecone index object which is used to query and upsert vectors
+ * 
+ * This function initializes the Pinecone database connection using the provided API key
+ * from environment variables and creates or accesses a database index named 
+ * "hack-the-north-challenge".
+ * 
+ * @returns {Promise<Pinecone.Index>} - A Pinecone index object
+ * 
+ */
 const configurePineconeDb = async () => {
   const pinecone = new Pinecone({
     apiKey: process.env.PINECONE_API_KEY,
@@ -14,6 +23,13 @@ const configurePineconeDb = async () => {
   return index;
 };
 
+/**
+ * Helper function to create a group and set the group for each user
+ * 
+ * @param {string} groupname group name to be created
+ * @param {[string]} usernames array of strings containing the usernames of the users to be grouped
+ * @returns {Promise<void>}
+ */
 const createGroup = async (groupname, usernames) => {
   if (usernames.length < 2) {
     res.status(400).send("Group must contain at least two users");
@@ -42,6 +58,15 @@ const createGroup = async (groupname, usernames) => {
   newGroup.save();
 };
 
+router.get("/", async (req, res) => {
+  const groups = await Group.find();
+  res.send(groups);
+});
+
+/**
+ * Route to group a single user with other users based on their skills similarity
+ * Expects a user ID as a URL parameter
+ */
 router.get("/groupIndividual/:id", async (req, res) => {
   const id = req.params.id;
   const currentUser = await User.findById(id);
@@ -71,6 +96,10 @@ router.get("/groupIndividual/:id", async (req, res) => {
   res.send(newGroup);
 });
 
+/**
+ * Route to iterate through all ungrouped users and find a group for them based on their skills similarity
+ * Does not expect a JSON body
+ */
 router.post("/group-ungrouped-users", async (req, res) => {
   const pineConeIndex = await configurePineconeDb();
   const ungroupedUsers = await User.find({ group: null });
@@ -95,6 +124,15 @@ router.post("/group-ungrouped-users", async (req, res) => {
   });
 });
 
+/**
+ * Route to create a group of users
+ * Expects a JSON body with the following format:
+ * {
+ *  "groupName": "group name",
+ *  "usernames": ["user1", "user2", "user3", "user4"]
+ * }
+ 
+ */
 router.post("/create-group", async (req, res) => {
   // expecting an array of usernames that will be grouped together
   const groupName = req.body.groupName;
@@ -102,16 +140,24 @@ router.post("/create-group", async (req, res) => {
   createGroup(groupName, group);
 });
 
-router.get("/", async (req, res) => {
-  const groups = await Group.find();
-  res.send(groups);
-});
-
+/**
+ * Route to find all groups that exist
+ * Returns an array of group objects in the following format:
+ * {
+ *  "groupName": "group name",
+ *  "groupMemberUsernames": ["user1", "user2", "user3", "user4"]
+ *  "submitted": "true/false"
+ * }
+ */
 router.get("/:groupName", async (req, res) => {
   const group = await Group.findOne({ groupName: req.params.groupName });
   res.send(group);
 });
 
+
+/**
+ * Route to find all users that don't have a group yet
+ */
 router.get("/ungrouped-users", async (req, res) => {
   const users = await User.find({ group: null });
   res.send(users);
