c838f5ec0542b2a5ea60a43badafc67ff5c5d54b
Benjamin Ng <60500272+benymng@users.noreply.github.com>
2024-02-20 22:31:54 -0500

    Added more tests for users

--- README.md
+++ README.md
@@ -1,25 +1,31 @@
 # Hack the North Backend Challenge README
 
+The API is hosted at [Hack the North Backend Challenge](https://htn-backend-challenge-3sno5bl95-benymng.vercel.app/).
+
 ## Overview
 
 In this application, I am using MongoDB, Express, Node and PineconeDB. In addition to the key functionality outlined in the challenge description for managing users and skills, I have created matching endpoints to handle group formationd during the hackathon.
 
 ## Group Formation Explanation
+
 One of the most common challenges especially for new hackers is the difficulty in finding a group of other students with similar interests, skills and skill level. Oftentimes, hackers are randomly assigned to other students and each of them want to utilize different tech stacks or have disparate interests. As a result, large compromises often must be made by one or more of the members in a team. Although this is an inevitable problem with all group projects, my goal was to find a better way to create groups which minimizes discrepencies between the skills between hackers. For the sake of demonstrating the functionality, I have done the matching based on the skills provided in the mock data provided, however, these same endpoints can easily be extended to include the interests, skill level and other key metrics for matching students. By leveraging a vector database, the matching of students is done drastically more efficiently than a traditional database and allows for a better matching of students based on their similarity. Moreover, the vector database can easily be extended to match mentors, friends or sponsors to students which would undoubtably improve the overall hackathon experience!
 
 ### Steps:
+
 1. **Vector Representation**
+
    - Each user's skills are represented as a high-dimensional vector. This vector is a numerical representation of the user's skills, where each dimension corresponds to a specific skill (1 means that the user has that skill and 0 means that the user does not).
 
 2. **Insertion into Pinecone**
+
    - When a new user is added via the `/new-user` endpoint, their skills vector is inserted into Pinecone. This is done using Pinecone's `upsert` method, which either inserts a new vector or updates an existing one.
 
 3. **Similarity Search**
+
    - When a new group is to be formed, a similarity search using the Euclidean distance is performed in Pinecone to find users with similar skills. This is done using Pinecone's `query` method, which returns the IDs of the most similar vectors in the database.
 
 4. **Group Formation**
    - The users corresponding to the most similar vectors are grouped together. This is done in the `/groups/create-group` endpoint, which creates a new group with the given users.
-
 
 ## File Structure
 
--- test/api.test.js
+++ test/api.test.js
@@ -1,33 +1,56 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../index.js');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../index.js");
 chai.use(chaiHttp);
 const should = chai.should();
 
 describe("API Endpoints", () => {
-    // Test for GET /
-    it("GET / should return the README", async () => {
-        const res = await chai.request(app).get('/');
-        res.should.have.status(200);
-    });
+  // Test for GET /
+  it("GET / should return the README", async () => {
+    const res = await chai.request(app).get("/");
+    res.should.have.status(200);
+  });
 
-    // Test for GET /users
-    it("GET /users should get all users", async () => {
-        const res = await chai.request(app).get('/users');
-        res.should.have.status(200);
-        res.body.should.be.a('array');
-    });
+  // Test for GET /users
+  it("GET /users should get all users", async () => {
+    const res = await chai.request(app).get("/users");
+    res.should.have.status(200);
+    res.body.should.be.a("array");
+  });
 
-    // Test for POST /new-user
-    it("POST /new-user should create a new user", async () => {
-        const user = {
-            name: "Test User",
-            email: "test@example.com",
-            skills: [{ skill: "JavaScript", rating: 5 }]
-        };
-        const res = await chai.request(app).post('/users/new-user').send(user);
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.property('name').eql('Test User');
-    });
+  // Test for POST /new-user
+  it("POST /new-user should create a new user", async () => {
+    const user = {
+      name: "Test User",
+      email: "test@example.com",
+      skills: [{ skill: "JavaScript", rating: 5 }],
+    };
+    const res = await chai.request(app).post("/users/new-user").send(user);
+    res.should.have.status(200);
+    res.body.should.be.a("object");
+    res.body.should.have.property("name").eql("Test User");
+  });
+
+  // Test for GET /users/:id
+  it("GET /users/id/:id should get a specific user", async () => {
+    const id = "65d11500d9b1a62b5393efd6";
+    const res = await chai.request(app).get(`/users/id/${id}`);
+    res.should.have.status(200);
+    res.body.should.be.a("object");
+    res.body.should.have.property("_id").eql(id);
+  });
+
+  // Test for PUT /users/:id
+  it("PUT /users/id/:id should update a specific user", async () => {
+    const id = "65d11500d9b1a62b5393efd6";
+    const user = {
+      name: "Updated User",
+      email: "updated@example.com",
+      skills: [{ skill: "Python", rating: 5 }],
+    };
+    const res = await chai.request(app).put(`/users/id/${id}`).send(user);
+    res.should.have.status(200);
+    res.body.should.be.a("object");
+    res.body.should.have.property("name").eql("Updated User");
+  });
 });
